674fda61cacfde180c16753ef838b0edaef0a9bc
TantariWD <46665152+KirativeWD@users.noreply.github.com>
2022-12-20 20:59:09 -0500

    Initialization commit

---
+++ resources/js/index.js
@@ -0,0 +1,18 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+      rating: ''
+    },
+    methods: {
+      submitForm: function() {
+        document.querySelector('form').classList.toggle('hidden');
+        document.querySelector('form').setAttribute('aria-hidden', 'true');
+        document.querySelector('.input-cta').setAttribute('tabindex', '-1');
+        document.querySelector('.btn').setAttribute('tabindex', '-1');
+        setTimeout(function() {
+          document.querySelector('.thank-you').classList.toggle('hidden');
+          document.querySelector('.thank-you').setAttribute('aria-hidden', 'false');
+        }, 1000);
+      }
+    }
+  });
